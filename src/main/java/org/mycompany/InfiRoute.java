b0d127c7333117ab59bd6aaf4c91cca1b96deb87
andreiiz <andrea.daskalopoulos@studio.unibo.it>
2022-07-26 14:29:21 +0200

    dd

--- src/main/java/org/mycompany/InfiRoute.java
+++ src/main/java/org/mycompany/InfiRoute.java
@@ -42,7 +42,7 @@ public class InfiRoute extends RouteBuilder {
 	    .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
 	    .setHeader(InfinispanConstants.KEY).constant("123")
 	   .setHeader(InfinispanConstants.VALUE).constant("hello")
-	    .to("infinispan://datagrid-external-datagrid.apps.integration.lab.local/sap-test?username=developer&password=TcrlVPRLsCyfFgWI");	 
+	    .to("infinispan:datagrid-external-datagrid.apps.integration.lab.local/sap-test:11222?username=developer&password=TcrlVPRLsCyfFgWI");	 
 	}
 
 }
