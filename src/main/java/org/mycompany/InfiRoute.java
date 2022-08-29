fb4ce6b074e3d072b94810ff622a50063b23a974
andreiiz <andrea.daskalopoulos@studio.unibo.it>
2022-08-29 16:25:21 +0200

    jsossssd

--- src/main/java/org/mycompany/InfiRoute.java
+++ src/main/java/org/mycompany/InfiRoute.java
@@ -3,6 +3,7 @@ package org.mycompany;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.infinispan.InfinispanConstants;
 import org.apache.camel.component.infinispan.InfinispanOperation;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 
@@ -12,17 +13,17 @@ public class InfiRoute extends RouteBuilder {
 	
 	@Override
 	public void configure() throws Exception {
-//		JSONObject jos = new JSONObject();
-//		jos.put("name", "jon doe");
-//		jos.put("age", "22");
-//		jos.put("city", "chicago");
+		JSONObject jos = new JSONObject();
+		jos.put("name", "jon doe");
+		jos.put("age", "22");
+		jos.put("city", "chicago");
 		
 		//from("timer://foo?repeatCount=1")
-		from("direct:provare").startupOrder(2)
-		.routeId("route2")
+		from("direct:provare") //.startupOrder(2)
+	//	.routeId("route2")
 			.setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
 			.setHeader(InfinispanConstants.KEY).constant("123456")
-		//	.setHeader(InfinispanConstants.VALUE).constant(setBody(String.class)) //era jos.tostring
+			.setHeader(InfinispanConstants.VALUE).constant(jos.toString()) //era jos.tostring
 			.to("infinispan:sap-test");
 	}
 }
