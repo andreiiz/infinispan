b60d1fa7cfe442c481e94daa44141f3d08159505
andreiiz <andrea.daskalopoulos@studio.unibo.it>
2022-08-29 16:08:43 +0200

    jsossss

--- src/main/java/org/mycompany/InfiRoute.java
+++ src/main/java/org/mycompany/InfiRoute.java
@@ -1,26 +1,10 @@
 package org.mycompany;
 
-
-import java.lang.module.Configuration;
-
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.infinispan.InfinispanConstants;
 import org.apache.camel.component.infinispan.InfinispanOperation;
-import org.apache.camel.language.Bean;
-import org.apache.camel.model.dataformat.JsonLibrary;
-import org.apache.camel.spi.Registry;
-import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Component;
 
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-
-import org.apache.camel.component.infinispan.*;
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.configuration.*;
-import org.json.JSONObject;
 
 
 @Component
@@ -34,7 +18,8 @@ public class InfiRoute extends RouteBuilder {
 //		jos.put("city", "chicago");
 		
 		//from("timer://foo?repeatCount=1")
-		from("direct:provare")
+		from("direct:provare").startupOrder(2)
+		.routeId("route2")
 			.setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
 			.setHeader(InfinispanConstants.KEY).constant("123456")
 			.setHeader(InfinispanConstants.VALUE).constant(body()) //era jos.tostring
